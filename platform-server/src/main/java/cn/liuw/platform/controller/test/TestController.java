7c3ee6c51dd53ddce7ac7233866b0d0c60063b28
liuw <liuw@pukkasoft.cn>
2021-01-04 09:28:28 +0800

    增加mongodb

--- platform-server/src/main/java/cn/liuw/platform/controller/test/TestController.java
+++ platform-server/src/main/java/cn/liuw/platform/controller/test/TestController.java
@@ -4,12 +4,18 @@ package cn.liuw.platform.controller.test;
 import cn.liuw.platform.common.base.controller.BaseController;
 import cn.liuw.platform.common.base.response.ResponseData;
 import cn.liuw.platform.db.domain.system.request.ValidObjectRequest;
+import cn.liuw.platform.db.domain.system.response.LoginUser;
 import cn.liuw.platform.service.test.TestService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author liuw
@@ -23,6 +29,9 @@ public class TestController extends BaseController {
 
     @Autowired
     private TestService testService;
+    
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     @ApiOperation(value = "排序")
     @GetMapping("/sort")
@@ -41,6 +50,28 @@ public class TestController extends BaseController {
     @ApiOperation(value = "post-测试")
     @PostMapping("/post")
     public ResponseData post() {
+        return success();
+    }
+
+    @ApiOperation(value = "mongo-测试")
+    @PostMapping("/mongo")
+    public ResponseData mongo() {
+        LoginUser loginUser = new LoginUser();
+        loginUser.setUsername("root");
+        
+        mongoTemplate.insert(loginUser, "user");
+        
+        return success();
+    }
+
+    @ApiOperation(value = "mongo-测试2")
+    @PostMapping("/mongo2")
+    public ResponseData mongo2() {
+        Query query = new Query(Criteria.where("username").is("ee"));
+        List<LoginUser> ans = mongoTemplate.find(query, LoginUser.class ,"user");
+        for (LoginUser an : ans) {
+            System.out.println(an.toString());
+        }
         return success();
     }
     
